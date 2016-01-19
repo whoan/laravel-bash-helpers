5c20264bf23e42e71c5d3ec26a8aafd34a9ec360
Juan Eugenio Abadie <abadiejuan@hotmail.com>
2016-01-19 19:14:21 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # artisan-finder
-Execute artisan from any Laravel subdirectory in Bash
+Execute artisan from any Laravel subdirectory in Bash.
+
+# Installation
+ - Append the script to your .bashrc:
+ 
+```bash
+cat artisan-finder.sh >> ~/.bashrc
+```
+ - Open a new terminal.
---
+++ artisan-finder.sh
@@ -0,0 +1,27 @@
+explode() {
+    local string=${1:?You must provide the string to explode}
+    local delimiter=${2:?You must provide the delimiter}
+    local -n out_array=${3:?You must provide an array to store the result}
+    unset out_array[*]
+    while [[ "$string" =~ $delimiter ]]; do
+        out_array+=( "${string%%$delimiter*}" )
+        string="${string#*$delimiter}"
+    done
+    out_array+=( "$string" )
+}
+
+artisan() {
+    directories=()
+    local path
+    explode "$PWD" "/" directories
+    while [ ${#directories[@]} -gt 0 ]; do
+        path="$( IFS=/ && printf '%s' "${directories[*]}" )/artisan"
+        [ -e "$path" ] && break
+        unset directories[-1]
+    done
+    if [ ${#directories[@]} -gt 0 ]; then
+        php "$path" "$@"
+    else
+        echo "Debe situarse en un proyecto Laravel para utilizar artisan" >&2
+    fi
+}
