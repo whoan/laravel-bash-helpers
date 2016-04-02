dc97b4adc8f765a0d302a760455f2f589e1a676b
Juan Eugenio Abadie <abadiejuan@hotmail.com>
2016-04-02 02:05:20 -0300

    added gulp locally

--- laravel-helpers.sh
+++ laravel-helpers.sh
@@ -1,3 +1,4 @@
+# helper
 explode() {
     local string=${1:?You must provide the string to explode}
     local delimiter=${2:?You must provide the delimiter}
@@ -10,18 +11,38 @@ explode() {
     out_array+=( "$string" )
 }
 
-artisan() {
+# helper
+findthis() {
     directories=()
     local path
     explode "$PWD" "/" directories
     while [ ${#directories[@]} -gt 0 ]; do
-        path="$( IFS=/ && printf '%s' "${directories[*]}" )/artisan"
-        [ -e "$path" ] && break
+        path="$( IFS=/ && printf '%s' "${directories[*]}" )/$*"
+        if [ -e "$path" ]; then
+            echo "$path"
+            return
+        fi
         unset directories[-1]
     done
-    if [ ${#directories[@]} -gt 0 ]; then
+    return 1
+}
+
+# call artisan from anywhere
+artisan() {
+    local path
+    if path=$(findthis artisan); then
         php "$path" "$@"
     else
         echo "You must be in a Laravel project in order to use artisan" >&2
+    fi
+}
+
+# no need gulp globally anymore
+gulp() {
+    local path
+    if path=$(findthis node_modules); then
+        "$path/gulp/bin/gulp.js" "$@"
+    else
+        echo "You must install gulp locally in order to use it (npm install gulp)" >&2
     fi
 }
