54aeb8ef9fe74d0f87aeed8a1eb14f0e555ff55d
Juan <jabadie@siacseguridad.com>
2016-04-08 11:49:26 -0300

    added "phpunit"; some clean

--- laravel-helpers.sh
+++ laravel-helpers.sh
@@ -1,5 +1,5 @@
-# helper
-explode() {
+
+__explode() {
     local string=${1:?You must provide the string to explode}
     local delimiter=${2:?You must provide the delimiter}
     local -n out_array=${3:?You must provide an array to store the result}
@@ -11,11 +11,10 @@ explode() {
     out_array+=( "$string" )
 }
 
-# helper
-findthis() {
+__find_file() {
     directories=()
     local path
-    explode "$PWD" "/" directories
+    __explode "$PWD" "/" directories
     while [ ${#directories[@]} -gt 0 ]; do
         path="$( IFS=/ && printf '%s' "${directories[*]}" )/$*"
         if [ -e "$path" ]; then
@@ -30,7 +29,7 @@ findthis() {
 # call artisan from anywhere
 artisan() {
     local path
-    if path=$(findthis artisan); then
+    if path=$(__find_file artisan); then
         php "$path" "$@"
     else
         echo "You must be in a Laravel project in order to use artisan" >&2
@@ -40,9 +39,23 @@ artisan() {
 # no need gulp globally anymore
 gulp() {
     local path
-    if path=$(findthis node_modules); then
+    if path=$(__find_file node_modules); then
         "$path/gulp/bin/gulp.js" "$@"
     else
         echo "You must install gulp locally in order to use it (npm install gulp)" >&2
+    fi
+}
+
+# call phpunit from anywhere
+phpunit() {
+    local path
+    if path=$(__find_file vendor); then
+        #( # uncomment this (and that*) if you dont wanna $OLDPWD changed. it executes the if body in a subshell, which is more expensive
+        cd "$path/.." 
+        vendor/bin/phpunit
+        cd - > /dev/null
+        #) # that*
+    else
+        echo "You must be in a project with phpunit in order to execute it" >&2
     fi
 }
